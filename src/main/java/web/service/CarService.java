c477fa19de30b04152e4f246fbfbf3d4fea4bf42
AllEgg <clodpupon@gmail.com>
2022-09-13 16:56:03 +0300

    Stream service realisation

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -8,26 +8,23 @@ import java.util.List;
 @Service
 public class CarService {
 
-    private List<Car> carlist;
+    private final List<Car> carList;
 
     {
-        carlist = new ArrayList<>();
-        carlist.add(new Car(12L, "BMW", 2345000.00));
-        carlist.add(new Car(2L, "Opel", 342300.00));
-        carlist.add(new Car(22L, "Mercedes", 13413400.00));
-        carlist.add(new Car(5L, "Jiguli", 432000.00));
-        carlist.add(new Car(3L, "Niva", 343400.00));
-
+        carList = new ArrayList<>();
+        carList.add(new Car(12L, "BMW", 2345000.00));
+        carList.add(new Car(2L, "Opel", 342300.00));
+        carList.add(new Car(22L, "Mercedes", 13413400.00));
+        carList.add(new Car(5L, "Jiguli", 432000.00));
+        carList.add(new Car(3L, "Niva", 343400.00));
     }
 
     public List<Car> getCar(Integer count) {
-        List<Car> resultList = new ArrayList<>();
-        if (count != null && count < 5) {
-            for (int i = 0; i < count; i++) {
-                resultList.add(carlist.get(i));
-            }
-            return resultList;
+        // тут сначала делал через цикл, потом передумал и сделал через стрим. Не знаю, что из этого
+        // оптимальнее, но стрим короче по записи. Буду благодарен за комментарий))
+        if (count != null && count < 5 && count > 0) {
+            return carList.stream().limit(count).toList();
         }
-        return carlist;
+        return carList;
     }
 }
